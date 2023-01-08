6dd670fd36bd0998295a725736ae79727339ba9d
Ellen Steiner <esteiner@c2s14d78.42wolfsburg.de>
2023-01-08 21:45:32 +0100

    adding two functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: esteiner <esteiner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 18:38:52 by esteiner          #+#    #+#             */
-/*   Updated: 2022/12/19 18:54:55 by esteiner         ###   ########.fr       */
+/*   Updated: 2023/01/08 19:36:49 by esteiner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <ctype.h>
 
 int		ft_atoi(char *str);
 void	*ft_bzero(void *s, size_t n);
@@ -29,5 +30,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *str, int c, size_t n);
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcat(char* restrict dst, const char* restrict src, size_t dstsize);
+int		ft_toupper(int c);
 
 #endif
